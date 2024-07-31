7023cd4b7d956930ce5a943f3299c7e0f52586ff
NASHID <kknashid0@gmail.com>
2024-07-31 08:46:03 +0530

    correct indention for both tabs and spaces using tabspace and 'insertSpaces'

--- src/indent.js
+++ src/indent.js
@@ -5,18 +5,26 @@ function indent(textEditor, edit, args) {
     // Check if the pressed key is 'Enter' and the file is JavaScript
     if (args.text === '\n' && textEditor.document.languageId === 'javascript' && textEditor.document.lineAt(0).text.includes("open")) {
 
-
         const position = textEditor.selection.active;
         const lineNumber = position.line;
         const currentLine = textEditor.document.lineAt(lineNumber);
         const currentLineText = currentLine.text;
-
+        
         if (currentLineText.trim().endsWith(':') || 
-            currentLineText.trim().endsWith('>')) {
+        currentLineText.trim().endsWith('>')) {
             
+            const tabSize = textEditor.options.tabSize;     // the number of spaces or tabs of user settings
+
             // Calculate the new indentation
-            const currentIndentation = currentLine.firstNonWhitespaceCharacterIndex;
-            const newIndentation = ' '.repeat(currentIndentation + 4);
+            const currentIndentation = currentLineText.slice(0,currentLine.firstNonWhitespaceCharacterIndex)
+            let newIndentation = ''
+            
+            if(textEditor.options.insertSpaces){    //if using spaces to indent
+                newIndentation = currentIndentation + ' '.repeat(tabSize)
+            } else {
+                newIndentation = '\t' + currentIndentation
+            }
+            
             edit.insert(position, '\n' + newIndentation);
 
             // Prevent the default 'Enter' behavior
